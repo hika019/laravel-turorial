7ef371a7cd96022446d09677d547580427534f6d
hika019 <hkvm584@gmail.com>
2024-01-06 16:55:29 +0900

    add error msg

--- blog/resources/views/articles/create.blade.php
+++ blog/resources/views/articles/create.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+@include('commons.errors')
     <form action="{{ route('articles.store') }}" method="post">
         @include('articles.form')
         <button type="submit">投稿する</button>
--- blog/resources/views/articles/form.blade.php
+++ blog/resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <dl class="form-list">
     <dt>タイトル</dt>
-    <dd><input type="text" name="title"></dd>
+    <dd><input type="text" name="title", value="{{ old('title') }}"></dd>
     <dt>本文</dt>
     <dd>
-        <textarea name="body" rows="5"></textarea>
+        <textarea name="body" rows="5">{{ old('body') }}</textarea>
     </dd>
 </dl>
